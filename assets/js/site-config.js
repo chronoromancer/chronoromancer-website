31ba0582b3f12f10f201fb50c4e04a0c811da79b
Chronoromancer <chronoromancerstudio@gmail.com>
2026-08-29 18:53:58 +0800

    v097b
    v097b

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,6 +1,6 @@
 window.CHRONO_SITE_CONFIG = Object.freeze({
-  release: "V0.9.7",
-  promotionRelease: "V0.9.7",
+  release: "V0.9.7b",
+  promotionRelease: "V0.9.7b",
   downloads: {
     itch: {
       url: "https://chronoromancer.itch.io/chronoromancer",
@@ -15,38 +15,38 @@ window.CHRONO_SITE_CONFIG = Object.freeze({
       enabled: true
     },
     directWin: {
-      url: "https://downloads.chronoromancergame.com/Chronoromancer-V0.9.7_Malice-win.zip",
+      url: "https://downloads.chronoromancergame.com/Chronoromancer-V0.9.7b_Malice-win.zip",
       enabled: true
     },
     directPc: {
-      url: "https://downloads.chronoromancergame.com/Chronoromancer-V0.9.7_Malice-pc.zip",
+      url: "https://downloads.chronoromancergame.com/Chronoromancer-V0.9.7b_Malice-pc.zip",
       enabled: true
     },
     directMac: {
-      url: "https://downloads.chronoromancergame.com/Chronoromancer-V0.9.7_Malice-mac.zip",
+      url: "https://downloads.chronoromancergame.com/Chronoromancer-V0.9.7b_Malice-mac.zip",
       enabled: true
     },
     directAndroid: {
-      url: "https://downloads.chronoromancergame.com/com.chronoromancer.game-9-1787942337-release.apk",
+      url: "https://downloads.chronoromancergame.com/com.chronoromancer.game-9-1787997339-release.apk",
       enabled: true
     },
     torrentPc: {
-      url: "assets/downloads/torrents/chronoromancer-v0.9.7-malice-pc-windows-linux.torrent",
-      magnet: "magnet:?xt=urn:btih:3c87d3fde95c9266f91300197f87739ddf0be347&dn=Chronoromancer-V0.9.7_Malice-pc.zip&ws=https%3A%2F%2Fdownloads.chronoromancergame.com%2FChronoromancer-V0.9.7_Malice-pc.zip&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce",
+      url: "assets/downloads/torrents/chronoromancer-v0.9.7b-malice-pc-windows-linux.torrent",
+      magnet: "magnet:?xt=urn:btih:08bd2c5a3fe567c580a00c2ad0ee002a514d58b6&dn=Chronoromancer-V0.9.7b_Malice-pc.zip&ws=https%3A%2F%2Fdownloads.chronoromancergame.com%2FChronoromancer-V0.9.7b_Malice-pc.zip&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce",
       enabled: true
     },
     torrentMac: {
-      url: "assets/downloads/torrents/chronoromancer-v0.9.7-malice-macos.torrent",
-      magnet: "magnet:?xt=urn:btih:c3035c8225cd534679d146be72abd3186aed3256&dn=Chronoromancer-V0.9.7_Malice-mac.zip&ws=https%3A%2F%2Fdownloads.chronoromancergame.com%2FChronoromancer-V0.9.7_Malice-mac.zip&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce",
+      url: "assets/downloads/torrents/chronoromancer-v0.9.7b-malice-macos.torrent",
+      magnet: "magnet:?xt=urn:btih:a4da655584727c9011c67bec71c72063c91df745&dn=Chronoromancer-V0.9.7b_Malice-mac.zip&ws=https%3A%2F%2Fdownloads.chronoromancergame.com%2FChronoromancer-V0.9.7b_Malice-mac.zip&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce",
       enabled: true
     },
     torrentAndroid: {
-      url: "assets/downloads/torrents/chronoromancer-v0.9.7-malice-android.torrent",
-      magnet: "magnet:?xt=urn:btih:63b6113b2764a275499c4dc945b18234c5f345f6&dn=com.chronoromancer.game-9-1787942337-release.apk&ws=https%3A%2F%2Fdownloads.chronoromancergame.com%2Fcom.chronoromancer.game-9-1787942337-release.apk&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce",
+      url: "assets/downloads/torrents/chronoromancer-v0.9.7b-malice-android.torrent",
+      magnet: "magnet:?xt=urn:btih:b11f2e60eaf4514c7d843dc257044a0e74bf1d61&dn=com.chronoromancer.game-9-1787997339-release.apk&ws=https%3A%2F%2Fdownloads.chronoromancergame.com%2Fcom.chronoromancer.game-9-1787997339-release.apk&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce",
       enabled: true
     },
     torrentChecksums: {
-      url: "assets/downloads/torrents/chronoromancer-v0.9.7-malice-sha256sums.txt",
+      url: "assets/downloads/torrents/chronoromancer-v0.9.7b-malice-sha256sums.txt",
       enabled: true
     }
   },
--- assets/js/site.js
+++ assets/js/site.js
@@ -146,7 +146,7 @@
 
   function initialiseConfiguredLinks() {
     document.querySelectorAll("[data-site-version]").forEach((element) => {
-      element.textContent = config.release || "V0.9.7";
+      element.textContent = config.release || "V0.9.7b";
     });
 
     document.querySelectorAll("[data-config-link]").forEach((element) => {
